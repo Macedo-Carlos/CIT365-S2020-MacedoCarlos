25b066738ceccca89784645b55dd27af101da70a
Macedo-Carlos <35361999+Macedo-Carlos@users.noreply.github.com>
2020-04-25 17:20:09 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CIT365_S2020_MacedoCarlos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
