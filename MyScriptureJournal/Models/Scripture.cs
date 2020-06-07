e53cf032a09055b10d8f3b027f8aab581000b950
Macedo-Carlos <35361999+Macedo-Carlos@users.noreply.github.com>
2020-06-06 23:37:04 -0600

    Scripture Journal

---
+++ MyScriptureJournal/Models/Scripture.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyScriptureJournal.Models
+{
+    public class Scripture
+    {
+        public int ID { get; set; }
+        public string Book { get; set; }
+        [Display(Name = "Date Added")]
+        [DataType(DataType.Date)]
+        public DateTime DateAdded { get; set; }
+        public int Chapter { get; set; }
+        public int Verse { get; set; }
+        public string Note { get; set; }
+
+    }
+}
---
+++ MyScriptureJournal/Pages/Scriptures/Index.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MyScriptureJournal.Models;
+
+namespace MyScriptureJournal.Pages.Scriptures
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MyScriptureJournal.Models.MyScriptureJournalContext _context;
+
+        public IndexModel(MyScriptureJournal.Models.MyScriptureJournalContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Scripture> Scripture { get;set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchKeywordString { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string BookSort { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string DateSort { get; set; }
+
+
+        public async Task OnGetAsync()
+        {
+            var scriptures = from m in _context.Scripture
+                         select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                scriptures = scriptures.Where(s => s.Book.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(SearchKeywordString))
+            {
+                scriptures = scriptures.Where(s => s.Note.Contains(SearchKeywordString));
+            }
+
+            if (!string.IsNullOrEmpty(BookSort))
+            {
+                scriptures = scriptures.OrderBy(s => s.Book);
+            }
+
+            if (!string.IsNullOrEmpty(DateSort))
+            {
+                scriptures = scriptures.OrderBy(s => s.DateAdded);
+            }
+
+            Scripture = await scriptures.ToListAsync();
+        }
+    }
+}
